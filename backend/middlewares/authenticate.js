44c70ec1d028b636ac2ac7828ee6cdb1d518cceb
ramk770 <ramanarayanankit2022@citchennai.net>
2024-11-20 12:52:43 +0530

    jwt error

--- backend/middlewares/authenticate.js
+++ backend/middlewares/authenticate.js
@@ -7,7 +7,7 @@ exports.isAuthenticatedUser = catchAsyncError( async (req, res, next) => {
    const { token  }  = req.cookies;
    
    if( !token ){
-        return next(new ErrorHandler('Login first to handle this resource', 401))
+        return next(new ErrorHandler('Login hello handle this resource', 401))
    }
 
    const decoded = jwt.verify(token, process.env.JWT_SECRET)
@@ -18,7 +18,7 @@ exports.isAuthenticatedUser = catchAsyncError( async (req, res, next) => {
 exports.authorizeRoles = (...roles) => {
    return  (req, res, next) => {
         if(!roles.includes(req.user.role)){
-            return next(new ErrorHandler(`Role ${req.user.role} is not allowed`, 401))
+            return next(new ErrorHandler(`Role ${req.user.role}  heloo allowed`, 401))
         }
         next()
     }
--- frontend/src/components/user/Register.js
+++ frontend/src/components/user/Register.js
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { register, clearAuthError } from '../../actions/userActions';
+import {useEffect, useState} from 'react';
+import {useDispatch, useSelector } from 'react-redux'
+import { register, clearAuthError } from '../../actions/userActions'
 import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
 
@@ -8,138 +8,128 @@ export default function Register() {
     const [userData, setUserData] = useState({
         name: "",
         email: "",
-        password: "",
-        role: "user" // default role as "user"
+        password: ""
     });
     const [avatar, setAvatar] = useState("");
     const [avatarPreview, setAvatarPreview] = useState("/images/default_avatar.png");
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const { loading, error, isAuthenticated } = useSelector(state => state.authState);
+    const { loading, error, isAuthenticated } = useSelector(state => state.authState)
 
     const onChange = (e) => {
-        if (e.target.name === 'avatar') {
-            const reader = new FileReader();
-            reader.onload = () => {
-                if (reader.readyState === 2) {
+        if(e.target.name === 'avatar') {
+           const reader = new FileReader();
+           reader.onload = () => {
+                if(reader.readyState === 2) {
                     setAvatarPreview(reader.result);
-                    setAvatar(e.target.files[0]);
+                    setAvatar(e.target.files[0])
                 }
-            };
-            reader.readAsDataURL(e.target.files[0]);
-        } else {
-            setUserData({ ...userData, [e.target.name]: e.target.value });
+           }
+
+
+           reader.readAsDataURL(e.target.files[0])
+        }else{
+            setUserData({...userData, [e.target.name]:e.target.value })
         }
-    };
+    }
 
     const submitHandler = (e) => {
         e.preventDefault();
         const formData = new FormData();
-        formData.append('name', userData.name);
-        formData.append('email', userData.email);
-        formData.append('password', userData.password);
-        formData.append('role', userData.role);
+        formData.append('name', userData.name)
+        formData.append('email', userData.email)
+        formData.append('password', userData.password)
         formData.append('avatar', avatar);
-        dispatch(register(formData));
-    };
+        dispatch(register(formData))
+    }
 
-    useEffect(() => {
-        if (isAuthenticated) {
+    useEffect(()=>{
+        if(isAuthenticated) {
             navigate('/');
-            return;
+            return
         }
-        if (error) {
+        if(error)  {
             toast(error, {
                 position: toast.POSITION.BOTTOM_CENTER,
                 type: 'error',
-                onOpen: () => { dispatch(clearAuthError()); }
-            });
+                onOpen: ()=> { dispatch(clearAuthError) }
+            })
+            return
         }
-    }, [error, isAuthenticated, dispatch, navigate]);
+    },[error, isAuthenticated, dispatch, navigate])
 
     return (
         <div className="row wrapper">
             <div className="col-10 col-lg-5">
-                <form onSubmit={submitHandler} className="shadow-lg" encType='multipart/form-data'>
-                    <h1 className="mb-3">Register</h1>
+            <form onSubmit={submitHandler} className="shadow-lg" encType='multipart/form-data'>
+                <h1 className="mb-3">Register</h1>
 
-                    <div className="form-group">
-                        <label htmlFor="name_field">Name</label>
-                        <input name='name' onChange={onChange} type="text" id="name_field" className="form-control" />
+            <div className="form-group">
+                <label htmlFor="email_field">Name</label>
+                <input name='name' onChange={onChange} type="name" id="name_field" className="form-control" />
+            </div>
+
+                <div className="form-group">
+                <label htmlFor="email_field">Email</label>
+                <input
+                    type="email"
+                    id="email_field"
+                    name='email' 
+                    onChange={onChange}
+                    className="form-control"
+                  
+                />
+                </div>
+    
+                <div className="form-group">
+                <label htmlFor="password_field">Password</label>
+                <input
+                    name='password' 
+                    onChange={onChange}
+                    type="password"
+                    id="password_field"
+                    className="form-control"
+                  
+                />
+                </div>
+
+                <div className='form-group'>
+                <label htmlFor='avatar_upload'>Avatar</label>
+                <div className='d-flex align-items-center'>
+                    <div>
+                        <figure className='avatar mr-3 item-rtl'>
+                            <img
+                                src={avatarPreview}
+                                className='rounded-circle'
+                                alt='Avatar'
+                            />
+                        </figure>
                     </div>
-
-                    <div className="form-group">
-                        <label htmlFor="email_field">Email</label>
+                    <div className='custom-file'>
                         <input
-                            type="email"
-                            id="email_field"
-                            name='email'
+                            type='file'
+                            name='avatar'
                             onChange={onChange}
-                            className="form-control"
+                            className='custom-file-input'
+                            id='customFile'
                         />
+                        <label className='custom-file-label' htmlFor='customFile'>
+                            Choose Avatar
+                        </label>
                     </div>
-
-                    <div className="form-group">
-                        <label htmlFor="password_field">Password</label>
-                        <input
-                            name='password'
-                            onChange={onChange}
-                            type="password"
-                            id="password_field"
-                            className="form-control"
-                        />
-                    </div>
-
-                    <div className="form-group">
-                        <label htmlFor="role_field">Role</label>
-                        <select
-                            name="role"
-                            id="role_field"
-                            onChange={onChange}
-                            className="form-control"
-                        >
-                            <option value="user">user</option>
-                            <option value="admin">admin</option>
-                        </select>
-                    </div>
-
-                    <div className="form-group">
-                        <label htmlFor="avatar_upload">Avatar</label>
-                        <div className="d-flex align-items-center">
-                            <div>
-                                <figure className="avatar mr-3 item-rtl">
-                                    <img
-                                        src={avatarPreview}
-                                        className="rounded-circle"
-                                        alt="Avatar"
-                                    />
-                                </figure>
-                            </div>
-                            <div className="custom-file">
-                                <input
-                                    type="file"
-                                    name="avatar"
-                                    onChange={onChange}
-                                    className="custom-file-input"
-                                    id="customFile"
-                                />
-                                <label className="custom-file-label" htmlFor="customFile">
-                                    Choose Avatar
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-
-                    <button
-                        id="register_button"
-                        type="submit"
-                        className="btn btn-block py-3"
-                        disabled={loading}
-                    >
-                        REGISTER
-                    </button>
-                </form>
+                </div>
+            </div>
+    
+                <button
+                id="register_button"
+                type="submit"
+                className="btn btn-block py-3"
+                disabled={loading}
+                >
+                REGISTER
+                </button>
+            </form>
             </div>
         </div>
-    );
+    )
 }
